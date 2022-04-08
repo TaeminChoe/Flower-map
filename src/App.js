6eadb1fca7c436852443af47334694f71e484d31
TaeminChoe <taeminteng@gmail.com>
2022-04-08 09:37:49 +0900

    loading sample code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
-  return (
-    <>
-      <h1>Hello</h1>
-      <h2>HI</h2>
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/components/Loading.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-image: url("imgs/main-image.jpg");
+  background-size: 100% 100%;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  animation-name: appear;
+  animation-duration: 4s;
+
+  @keyframes appear {
+    0% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0.5;
+    }
+  }
+`;
+
+const Loading = () => {
+  return <Container></Container>;
+};
+
+export default Loading;
