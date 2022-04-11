c34c55f1f8f6550336e2497390203e479b34a465
TaeminChoe <taeminteng@gmail.com>
2022-04-11 13:31:45 +0900

    link loading and main

--- src/pages/contentPage.js
+++ src/pages/contentPage.js
@@ -1,9 +1,10 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MediaQuery, { useMediaQuery } from "react-responsive";
 // components
 import Header from "../components/header";
 import FlowerSlide from "../components/flowerSlide";
 import MapArea from "../components/mapArea";
+import Loading from "./Loading";
 //css
 import { StyledWrap } from "../css/styledWrap";
 import { StyledMain } from "../css/styledMain";
@@ -21,6 +22,7 @@ function ContentPage() {
   });
 
   const query = useRef();
+  const [loading, setLoading] = useState(true);
 
   if (isPc) {
     query.current = "pc";
@@ -28,6 +30,17 @@ function ContentPage() {
     query.current = "tablet";
   } else if (isMobile) {
     query.current = "mobile";
+  }
+
+  /** 로딩 화면 지연 */
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  if (loading) {
+    return <Loading />;
   }
 
   return (
