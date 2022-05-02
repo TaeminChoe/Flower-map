9332ed03e0aff998740d65a287e86873158ec5cb
Your Name <you@example.com>
2022-05-02 17:06:38 +0900

    update :: Loading page layout

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,14 +1,29 @@
 import styled from "styled-components";
+import loFlower from "../images/loFlower.jpg";
 
 const Container = styled.div`
-  background-image: url("imgs/main-image.jpg");
-  background-size: 100% 100%;
-  position: absolute;
+  background-image: url(${loFlower});
   width: 100%;
   height: 100vh;
+  background-size: 100% 100%;
+  background-size: cover;
   animation-name: appear;
   animation-duration: 3s;
-
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Libre Bodoni", serif;
+  flex-flow: column;
+  div {
+    color: white;
+    font-size: 5rem;
+    margin: 1rem;
+  }
+  p {
+    font-size: 1.5rem;
+    color: white;
+    margin: 1rem;
+  }
   @keyframes appear {
     0% {
       opacity: 0;
@@ -23,7 +38,12 @@ const Container = styled.div`
 `;
 
 const Loading = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <div>FLOWER MAP</div>
+      <p>Now Loading . . .</p>
+    </Container>
+  );
 };
 
 export default Loading;
