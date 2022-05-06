6f6ffed490d81aec0759915b66d4ebed02824bf1
Your Name <you@example.com>
2022-05-06 13:28:11 +0900

    update layout

--- src/css/StyledDetailArea.js
+++ src/css/StyledDetailArea.js
@@ -8,7 +8,7 @@ const StyledDetailArea = styled.section`
   justify-content: center;
   align-items: center;
   margin: 1rem 0;
-  padding: 1rem 0;
+  padding: 2rem 0;
   border-radius: 10px;
   @media screen and (min-width: 1024px) {
     width: 45%;
