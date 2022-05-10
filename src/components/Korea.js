2147594a0f54a9771129fdf4c9276a15ce0e1053
Your Name <you@example.com>
2022-05-10 09:07:26 +0900

    update pr

--- src/components/Korea.js
+++ src/components/Korea.js
@@ -8,26 +8,9 @@ import { regionState } from "../atom";
 import { REGION_LIST } from "../utils/regionData";
 
 function Korea({ name }) {
-  console.log(name);
+  // console.log(name);
   const setRegion = useSetRecoilState(regionState);
   const navi = useNavigate();
-
-  /** 맵 선택시 className으로 구분 */
-  const mapClick = (e) => {
-    const id = REGION_LIST.find(
-      (region) => region.name === e.target.className
-    ).id;
-    navi(`/detail/${id}`);
-    setRegion(REGION_LIST.find((region) => region.id === Number(id)));
-  };
-
-  /** 지역이름 선택시 id로 구분 */
-  const stringClick = (e) => {
-    const id = REGION_LIST.find((region) => region.name === e.target.id).id;
-    navi(`/detail/${id}`);
-    setRegion(REGION_LIST.find((region) => region.id === Number(id)));
-    console.log(id);
-  };
 
   /*hover event :: 지역이름 호버시 해당 지역만 색 변화가 있게 */
   const hoverEvent = (e, boolean) => {
@@ -36,23 +19,37 @@ function Korea({ name }) {
       : (e.target.previousSibling.style.backgroundColor = "#eee");
   };
 
+  /** 맵 선택시 className으로 구분 */
+  const mapClick = (e) => {
+    const regionId = REGION_LIST.find(
+      (region) => region.name === e.target.className
+    ).id;
+    // console.log("id", regionId);
+    navi(`/detail/${regionId}`);
+    setRegion(REGION_LIST.find((region) => region.id === Number(regionId)));
+    // console.log(REGION_LIST.find((region) => region.id === Number(regionId)));
+  };
+
+  /** 지역이름 선택시 id로 구분 */
+  const stringClick = (e) => {
+    const regionId = REGION_LIST.find(
+      (region) => region.name === e.target.id
+    ).id;
+    navi(`/detail/${regionId}`);
+    setRegion(REGION_LIST.find((region) => region.id === Number(regionId)));
+  };
+
   return (
     <StyledRegion>
       {REGION_LIST &&
         REGION_LIST.map((region) => {
-          console.log(region.flowers, name);
+          // console.log(region.flowers, name);
           // name과 region.flowers의 값(벚꽃, 개나리, 진달래)이 같은 것에 개화날짜 뿌려주어야 함
           return (
-            <>
-              <div
-                key={region.id + "_map"}
-                id={region.id + "_map"}
-                className={region.name}
-                onClick={mapClick}
-              ></div>
+            <React.Fragment key={region.id + "_map"}>
+              <div className={region.name} onClick={mapClick}></div>
               <p
                 id={region.name}
-                key={region.id + "_string"}
                 className={region.name + "-string region-string"}
                 onClick={stringClick}
                 onMouseEnter={(e) => hoverEvent(e, true)}
@@ -60,7 +57,7 @@ function Korea({ name }) {
               >
                 {region.korName + " 3.25"}
               </p>
-            </>
+            </React.Fragment>
           );
         })}
     </StyledRegion>
